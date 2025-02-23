acd83d06b045c82de7f1d9f47c0961daaaa7a507
xiaosenho <helloworldzxj@163.com>
2025-02-23 15:53:56 +0800

    验证码模块：导入配置

---
+++ xiaosen-school-checkcode/src/test/java/com/xiaosenho/checkcode/XuechengPlusCheckcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaosenho.checkcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengPlusCheckcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
