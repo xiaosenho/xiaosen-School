0b6e0c01fd5fb11eafb73af8d62416a2546e0b84
xiaosenho <helloworldzxj@163.com>
2025-02-24 16:40:14 +0800

    选课学习模块：模块导入

---
+++ xiaosen-school-learning/xiaosen-school-learning-service/src/main/java/com/xiaosenho/learning/mapper/XcCourseTablesMapper.java
@@ -0,0 +1,15 @@
+package com.xiaosenho.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaosenho.learning.model.po.XcCourseTables;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcCourseTablesMapper extends BaseMapper<XcCourseTables> {
+
+}
